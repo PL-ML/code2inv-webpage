7df2b0b6a0dc2bf76a16ffd95b8ea9fba0a39843
Aaditya Naik <aadi.naik@gmail.com>
2020-05-07 03:59:34 +0530

    Restructured Added more detail Added some instructions in getting started

--- server.js
+++ server.js
@@ -10,5 +10,37 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
 
 app.get('/', function(req, res) {
+    res.redirect('/home')
+})
+
+app.get('/home', function(req, res) {
     res.render('pages/index')
+})
+
+app.get('/getting_started', function(req, res) {
+    res.render('pages/getting_started')
+})
+
+app.get('/features', function(req, res) {
+    res.render('pages/features')
+})
+
+app.get('/people', function(req, res) {
+    res.render('pages/people')
+})
+
+app.get('/publications', function(req, res) {
+    res.render('pages/publications')
+})
+
+app.get('/sponsors', function(req, res) {
+    res.render('pages/sponsors')
+})
+
+app.get('/demo', function(req, res) {
+    res.render('pages/demo')
+})
+
+app.get('*', function(req, res) {
+    res.render('pages/404')
 })
